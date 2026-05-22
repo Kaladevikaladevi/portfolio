ec405dab447327d7cd0fd1a987a18c9e56e52614
=Kaladevi N S <kaladevi9@gmail.com>
2026-05-22 12:55:38 +0530

    fixed vercel build errors

--- src/app/Latepro/page.jsx
+++ src/app/Latepro/page.jsx
@@ -1,11 +1,21 @@
 'use client';
 
+// import Latepro from "@/components/Latepro/Latepro";
+
+// export default function Latepro() {
+//     return (
+//         <div>
+//             <Latepro />
+//         </div>
+//     );
+// }
+
 import Latepro from "@/components/Latepro/Latepro";
 
-export default function Latepro() {
-    return (
-        <div>
-            <Latepro />
-        </div>
-    );
+export default function Page() {
+  return (
+    <div>
+      <Latepro />
+    </div>
+  );
 }
--- src/components/Latepro/Latepro.jsx
+++ src/components/Latepro/Latepro.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "./latepro.css";
+import './latepro.css';
 import { motion } from "framer-motion";
 import Image from "next/image";
 
